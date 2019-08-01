f40cbe5099be027809571d6ea3ae4db8fb062968
kangswx <996334837@qq.com>
2019-08-01 18:15:53 +0800

    添加注释

--- src/main/java/com/kangswx/springbootmybatisredis/controller/UserController.java
+++ src/main/java/com/kangswx/springbootmybatisredis/controller/UserController.java
@@ -12,21 +12,41 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    /**
+     * 查询接口
+     * @param id
+     * @return
+     */
     @GetMapping()
     public Object getById(int id){
         return userService.getByid(id);
     }
 
+    /**
+     * 修改接口
+     * @param user
+     * @return
+     */
     @PutMapping()
     public Object updateUserById(@RequestBody User user){
         return userService.updateUser(user);
     }
 
+    /**
+     * 添加接口
+     * @param user
+     * @return
+     */
     @PostMapping()
     public Object addUser(@RequestBody User user){
         return userService.addUser(user);
     }
 
+    /**
+     * 删除接口
+     * @param id
+     * @return
+     */
     @DeleteMapping()
     public Object deleteUser(int id){
         return userService.deleteUserById(id);
